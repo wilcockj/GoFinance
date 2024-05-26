cbc5cd5cb4e4dd146e1d11f79549871edd535770
James Wilcock <jameswilcock8@gmail.com>
2024-05-25 18:25:12 -0700

    added pretax and aftertax config

--- main.go
+++ main.go
@@ -12,13 +12,24 @@ type Investment struct {
 	balance              decimal.Decimal
 	monthly_contribution decimal.Decimal
 	expected_return      decimal.Decimal
+	pretax               bool
 }
 
 type Assets struct {
-	investments      []Investment
-	monthly_income   int64
-	monthly_expenses int64
+	investments           []Investment
+	monthly_pretax_income int64
+	monthly_income        Income
+	monthly_expenses      int64
+	months_invested       int
 }
+
+type Income struct {
+	pretax   int64
+	aftertax int64
+}
+
+// add a pre tax income and post tax income field
+// add a mark for investment if it is from pretax income
 
 func get_expenses(config map[string]interface{}) float64 {
 	expenses, ok := config["expenses"].(map[interface{}]interface{})
@@ -73,6 +84,11 @@ func get_investments_total_balance(config map[string]interface{}) []Investment {
 		}
 
 		new_investment.name = key.(string)
+		if new_investment.name == "401k" {
+			new_investment.pretax = true
+		} else {
+			new_investment.pretax = false
+		}
 		new_investment.balance = decimal.NewFromFloat(balance)
 
 		expected, ok := account["expected_return"].(float64)
@@ -106,24 +122,21 @@ func print_investments(investments []Investment) {
 
 }
 
-func get_income(config map[string]interface{}) int64 {
+func get_income(config map[string]interface{}) Income {
+	var income Income
 	expenses, ok := config["income"].(map[interface{}]interface{})
 	if !ok {
 		fmt.Println("income")
-		return 0.0
+		return income
 	}
 
-	var income int64 = 0.0
-	for _, amount := range expenses {
-		switch v := amount.(type) {
-		case int:
-			income += int64(v)
-		case int64:
-			income += int64(v)
-		default:
-			fmt.Printf("Unexpected type %T for value %v\n", v, v)
-		}
-	}
+	incomes, ok := expenses["job"].(map[interface{}]interface{})
+
+	pretax_income := int64(incomes["pretax"].(int))
+	aftertax_income := int64(incomes["aftertax"].(int))
+
+	income.pretax = pretax_income
+	income.aftertax = aftertax_income
 
 	return income
 }
@@ -135,7 +148,7 @@ func step_investments(assets *Assets, num_months int) {
 			(*investments)[i].balance = (*investments)[i].balance.Add((*investments)[i].balance.Mul((*investments)[i].expected_return).Div((decimal.NewFromInt(12))))
 			(*investments)[i].balance = (*investments)[i].balance.Add((*investments)[i].monthly_contribution)
 			if (*investments)[i].name == "cash" {
-				net_income := decimal.NewFromInt(assets.monthly_income - assets.monthly_expenses)
+				net_income := decimal.NewFromInt(assets.monthly_income.aftertax - assets.monthly_expenses)
 				fmt.Printf("added %f to cash\n", net_income.InexactFloat64())
 				// need to subtract monthly contributions
 				// or add that to expenses
@@ -143,8 +156,17 @@ func step_investments(assets *Assets, num_months int) {
 
 			}
 			if (*investments)[i].name == "401k" {
-				(*investments)[i].balance = (*investments)[i].balance.Add(decimal.NewFromFloat(float64(assets.monthly_income) * float64(.085)))
+				(*investments)[i].balance = (*investments)[i].balance.Add(decimal.NewFromFloat(float64(assets.monthly_income.pretax) * float64(.085)))
 			}
+		}
+		assets.months_invested++
+		fmt.Printf("months invested = %d\n", assets.months_invested)
+		if (assets.months_invested+1)%12 == 0 {
+			// yearly income increase of 2%
+			fmt.Printf("income changed from %d\n", assets.monthly_income.aftertax)
+			assets.monthly_income.aftertax = int64(float64(assets.monthly_income.aftertax) * float64(1.02))
+			fmt.Printf("to %d\n", assets.monthly_income.aftertax)
+
 		}
 	}
 }
@@ -171,14 +193,14 @@ func main() {
 
 	var investments []Investment = get_investments_total_balance(obj)
 	for _, investment := range investments {
-		if investment.name != "401k" {
+		if !investment.pretax {
 			tot_expenses += investment.monthly_contribution.InexactFloat64()
 		}
 	}
 	income := get_income(obj)
 
 	my_assets := Assets{investments: investments, monthly_expenses: int64(tot_expenses), monthly_income: income}
-	step_investments(&my_assets, 36)
+	step_investments(&my_assets, 14)
 
 	fmt.Printf("Got total expenses per month of %f\n\n", tot_expenses)
 
